6b274cd819dd74f826e0d44d702cbeab88ffc80c
Maas Dianto <maas.dianto@gmail.com>
2020-05-28 12:00:29 +0800

    variables in rust

---
+++ variables/src/main.rs
@@ -0,0 +1,24 @@
+fn main() {
+    // mutable
+    let mut x = 5;
+    println!("The value of x is {}", x);
+    x = 6;
+    println!("The value of x is {}", x);
+
+    // constants
+    const MAX_POINTS: u32 = 100_000;
+    println!("The value of const MAX_POINTS is {}", MAX_POINTS);
+
+    // shadowing
+    let x = 5;
+
+    let x = x + 1;
+
+    let x = x * 2;
+
+    println!("The value of x is {}", x);
+
+    let spaces = "    ";
+    let spaces = spaces.len();
+    println!("The value of spaces is {}", spaces);
+}
