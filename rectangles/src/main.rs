ff37261c2409346db3104c4417fd61a7fcbf26b5
Maas Dianto <maas.dianto@gmail.com>
2020-06-02 17:02:00 +0800

    add struct method and associate fn

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -1,7 +1,23 @@
 #[derive(Debug)]
 struct Rectangle {
-    width: i32,
-    height: i32
+    width: u32,
+    height: u32
+}
+
+impl Rectangle {
+    fn square(size: u32) -> Rectangle {
+        Rectangle {
+            width: size,
+            height: size
+        }
+    }
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
 }
 
 fn main() {
@@ -25,6 +41,25 @@ fn main() {
     // pretty debug
     println!("rect2 is {:#?}", rect2);
     println!("The area of rectangle is {} square pixels.", area_v3(&rect2));
+
+    // v4: use struct impl
+    println!("The area of rectangle is {} square pixels.", rect2.area());
+
+    let rect3 = Rectangle {
+        width:20,
+        height:40
+    };
+
+    let rect4 = Rectangle {
+        width: 40,
+        height: 50
+    };
+
+    println!("rect2 can hold rect3: {}", rect2.can_hold(&rect3));
+    println!("rect2 can hold rect4: {}", rect2.can_hold(&rect4));
+
+    // associate function
+    let rect5 = Rectangle::square(30);
 }
 
 fn area_v1(width: i32, height: i32) -> i32 {
