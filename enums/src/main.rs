a97ebd22ab17c0f173f568a913194c3187e30ff8
Maas Dianto <maas.dianto@gmail.com>
2020-06-04 19:11:16 +0800

    enum match

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -29,3 +29,22 @@ impl Message {
 
     }
 }
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => {
+            println!("Lucky penny!");
+            1
+        },
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter => 25,
+    }
+}
